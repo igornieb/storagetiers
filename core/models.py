e3a39cd1b8b6e8f9cb4c4305d67995d84c77cf59
igornieb <igornieb@gmail.com>
2023-02-18 16:08:02 +0100

    added get_sizes method to Tier model (returns allowed heights)

--- core/models.py
+++ core/models.py
@@ -3,6 +3,18 @@ from core.utilis import validate_sizes_allowed
 
 
 class Tier(models.Model):
+    def __str__(self):
+        return self.name
+
+    def get_sizes(self):
+        # returns list of allowed heights
+        val_list = str(self.sizes_allowed).split(" ")
+        res = []
+        for val in val_list:
+            if val.isdigit():
+                res.append(int(val))
+        return res
+
     name = models.CharField(max_length=100)
     sizes_allowed = models.CharField(max_length=100, validators=[validate_sizes_allowed])
     store_original = models.BooleanField(default=False)
