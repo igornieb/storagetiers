fda74132bc85f681c285f4efe5b09792780221a6
igornieb <igornieb@gmail.com>
2023-02-18 20:20:46 +0100

    created PictureList view

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from api import views
 
 urlpatterns = [
-
+    path('pictures', views.PictureList.as_view(), name='picture-list')
     ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from core.models import Picture, Account
+from api.serializers import *
 
-# Create your views here.
+
+# TODO add picture, picture details (links, etc)
+class PictureList(ListAPIView):
+    serializer_class = PictureSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        account = Account.objects.get(user=self.request.user)
+        pictures = Picture.objects.filter(owner=account)
+        return pictures
