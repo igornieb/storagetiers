02e3dc079d570ed2b050dfe65bf3b5c1f31c0de3
igornieb <igornieb@gmail.com>
2023-02-18 16:05:37 +0100

    added function create_base_tiers, creates base tiers after each migration

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+def create_base_tiers(**kwargs):
+    from core.models import Tier
+    try:
+        Tier.objects.all()
+    except:
+        pass
 
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        post_migrate.connect(create_base_tiers, sender=self)
