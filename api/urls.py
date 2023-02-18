0f06ad803b3edd89b257d27c10ede84fa54000c8
igornieb <igornieb@gmail.com>
2023-02-18 20:53:05 +0100

    created PictureDetails view

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from api import views
 
 urlpatterns = [
-    path('pictures', views.PictureList.as_view(), name='picture-list')
+    path('pictures', views.PictureList.as_view(), name='picture-list'),
+    path('picture/<pk>', views.PictureDetails.as_view(), name='picture-details')
     ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
+from django.http import Http404
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework import permissions
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from core.models import Picture, Account
 from api.serializers import *
 
@@ -14,3 +18,18 @@ class PictureList(ListAPIView):
         account = Account.objects.get(user=self.request.user)
         pictures = Picture.objects.filter(owner=account)
         return pictures
+
+class PictureDetails(APIView):
+    def get_permissions(self):
+        return [permissions.IsAuthenticated()]
+
+    def get_object(self, pk):
+        try:
+            return Picture.objects.get(pk=pk)
+        except Picture.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        picture = self.get_object(pk)
+        serializer = PictureSerializer(picture, many=False)
+        return Response(serializer.data)
