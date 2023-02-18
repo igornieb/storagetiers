5c00bd8dfaf50ddfb2c571b7637d5012bc191da5
igornieb <igornieb@gmail.com>
2023-02-18 16:36:28 +0100

    created Picture model

--- core/admin.py
+++ core/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from core.models import *
 
 @admin.register(Tier)
-class CategoryAdmin(admin.ModelAdmin):
+class TierAdmin(admin.ModelAdmin):
     list_display = ('name',)
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('user','tier',)
+
+@admin.register(Picture)
+class PictureAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'owner',)
--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 from core.utilis import validate_sizes_allowed
+from storagetiers.settings import MEDIA_ROOT
 
 
 class Tier(models.Model):
@@ -19,3 +21,25 @@ class Tier(models.Model):
     sizes_allowed = models.CharField(max_length=100, validators=[validate_sizes_allowed])
     store_original = models.BooleanField(default=False)
     allow_link = models.BooleanField(default=False)
+
+
+class Account(models.Model):
+    # this model extends built-in User model adding tier column\
+
+    def __str__(self):
+        return f"{self.user.username} - {self.tier}"
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tier = models.ForeignKey(Tier, on_delete=models.CASCADE)
+
+
+class Picture(models.Model):
+    # this model stores user uploaded pictures
+    def __str__(self):
+        return f"{self.pk} - {self.owner}"
+
+    def upload_to(self, filename):
+        return f"{MEDIA_ROOT}/{self.owner.user}/{filename}"
+
+    owner = models.ForeignKey(Account, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to=upload_to,)
