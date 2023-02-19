073e19c33dd7cde914978fc53a9112fa6a35d31e
igornieb <igornieb@gmail.com>
2023-02-19 20:10:21 +0100

    created post action in PictureList for adding new Pictures

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     path('pictures', views.PictureList.as_view(), name='picture-list'),
     path('picture/<pk>', views.PictureDetails.as_view(), name='picture-details'),
     path('picture/<pk>/<int:height>', views.PictureDetails.as_view(), name='picture-details')
-    ]
+]
