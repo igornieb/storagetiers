a366ce463978f14e5c5b9b65946ae8772e2f5ee8
igornieb <igornieb@gmail.com>
2023-02-19 18:15:02 +0100

    added PictureDetials view

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from api import views
 
 urlpatterns = [
     path('pictures', views.PictureList.as_view(), name='picture-list'),
-    path('picture/<pk>', views.PictureDetails.as_view(), name='picture-details')
+    path('picture/<pk>', views.PictureDetails.as_view(), name='picture-details'),
+    path('picture/<pk>/<int:height>', views.PictureDetails.as_view())
     ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,14 @@
-from django.http import Http404
+from django.http import Http404, FileResponse, HttpResponse
 from django.shortcuts import render
 from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from core.models import Picture, Account
 from api.serializers import *
+from PIL import Image
 
 
 # TODO add picture, picture details (links, etc)
@@ -25,11 +27,28 @@ class PictureDetails(APIView):
 
     def get_object(self, pk):
         try:
-            return Picture.objects.get(pk=pk)
+            account = Account.objects.get(user=self.request.user)
+            return Picture.objects.get(pk=pk, owner=account)
         except Picture.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk):
+    def get(self, request, pk, height=None):
         picture = self.get_object(pk)
-        serializer = PictureSerializer(picture, many=False)
-        return Response(serializer.data)
+        if height:
+            if height in picture.get_sizes():
+                image = Image.open(picture.img)
+                image.thumbnail((image.width,height), Image.ANTIALIAS)
+                response = HttpResponse(content_type='image/jpg')
+                image.save(response, "JPEG")
+                return response
+            else:
+                raise PermissionDenied({"message": "This account doesnt have permisions to acces this link",
+                                        "object": picture})
+        else:
+            if 'original' in picture.get_sizes():
+                return HttpResponse(picture.img, content_type="image/png")
+            else:
+                raise PermissionDenied({"message": "This account doesnt have permisions to acces this link",
+                                        "object": picture})
+
+
