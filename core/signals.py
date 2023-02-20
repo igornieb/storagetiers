312769a031573a34d709bd553b1db438a4860136
igornieb <igornieb@gmail.com>
2023-02-20 15:57:28 +0100

    added signal that creates base user on user create

--- core/signals.py
+++ core/signals.py
@@ -1,3 +1,10 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from core.models import Picture, Account
+from core.models import Account, User, Tier
+
+
+@receiver(post_save, sender=User)
+def create_account(sender, instance, created, **kwargs):
+    if created:
+        tier = Tier.objects.get(name="Basic")
+        Account.objects.create(user=instance, tier=tier)
