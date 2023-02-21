4ecf272874e62425acd4916166a4d9838eccf25e
igornieb <igornieb@gmail.com>
2023-02-21 15:02:19 +0100

    Added JWT token authentication

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from api import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('pictures', views.PictureList.as_view(), name='picture-list'),
@@ -7,4 +8,6 @@ urlpatterns = [
     path('picture/<uuid:pk>', views.PictureDetails.as_view(), name='picture-details'),
     path('picture/<uuid:pk>/<int:height>', views.PictureDetails.as_view(), name='picture-details'),
     path('timelink/<uuid:pk>', views.TimePictureDetails.as_view(), name='timelink'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
