e392fedcddf9775495789afc5d6d8f135561a855
igornieb <igornieb@gmail.com>
2023-02-21 13:45:29 +0100

    added uuid validation to urls

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from api import views
 urlpatterns = [
     path('pictures', views.PictureList.as_view(), name='picture-list'),
     path('pictures/shared', views.TimePictureList.as_view(), name='shared-picture-list'),
-    path('picture/<pk>', views.PictureDetails.as_view(), name='picture-details'),
-    path('picture/<pk>/<int:height>', views.PictureDetails.as_view(), name='picture-details'),
-    path('timelink/<pk>', views.TimePictureDetails.as_view(), name='timelink'),
+    path('picture/<uuid:pk>', views.PictureDetails.as_view(), name='picture-details'),
+    path('picture/<uuid:pk>/<int:height>', views.PictureDetails.as_view(), name='picture-details'),
+    path('timelink/<uuid:pk>', views.TimePictureDetails.as_view(), name='timelink'),
 ]
