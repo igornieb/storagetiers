368d3680d53cd6737c97bf691639c7d5256cef7e
igornieb <igornieb@gmail.com>
2023-02-21 18:43:14 +0100

    added rest_framework.authtoken to installed apps

--- storagetiers/settings.py
+++ storagetiers/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'core',
     'api',
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
