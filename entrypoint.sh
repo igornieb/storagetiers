3a3eac6b2a5af2924078d70177ec500b6b7c12a0
igornieb <igornieb@gmail.com>
2023-03-09 10:34:45 +0100

    celery scheduler now starts with container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-python manage.py makemigrations
-python manage.py migrate
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+celery -A storagetiers beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler --detach
 exec "$@"
